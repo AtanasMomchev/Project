529071d9c189b84d83171dffbc1d6ae0a6234c98
Unknown <a.momchev1@gmail.com>
2018-04-04 12:41:48 +0300

    method export and changes to interfaces

--- warehouse/src/controller/wareHouseController.java
+++ warehouse/src/controller/wareHouseController.java
@@ -4,10 +4,12 @@ package controller;
 import dao.LotsDAO;
 import dao.ProductsDAO;
 import dao.StockDAO;
+import exceptions.NotEnoughtProductsInSupply;
 import exceptions.NotEnoughtSpaceException;
 import exceptions.WarehouseExceptions;
 import model.Lot;
 import model.Product;
+import model.Stock;
 
 import java.sql.SQLException;
 import java.util.ArrayList;
@@ -21,19 +23,34 @@ public class wareHouseController {
         int ProductsTotalSize = product.getSize() * quantity;
         double ProductsTotalWeight = product.getWeight() * quantity;
         int availableSize = ld.totalSize() - sd.totalTakenSize();
-        double availableWeight = ld.totalWeight() - sd.totalTakenSize();
-
+        double availableWeight = ld.totalWeight() - sd.totalTakenWeight();
+        //check if there is enough space in the warehouse for the import
         if(ProductsTotalSize > availableSize || ProductsTotalWeight > availableWeight)
             throw new NotEnoughtSpaceException("Import package to large!");
-
+        //result is filled with lots_id in which the product is placed
         ArrayList<Integer> result = new ArrayList<>();
         return result = checkForFreeSpace(product, quantity);
     }
 
-    public void exportProduct(String name, int quantity) throws WarehouseExceptions, SQLException {
+    public ArrayList<Stock> exportProduct(String name, int quantity) throws WarehouseExceptions, SQLException {
+        StockDAO sd = new StockDAO();
         Product p = new ProductsDAO().findByName(name);
-        while (StockDao().getProductInLots(name).hasNext()){
+        //check if there is enough products in the warehouse to export
+        if(sd.productQuantityInStock(name) < quantity) throw new NotEnoughtProductsInSupply("Insufficient quantity in supply!");
+
+        ArrayList<Stock> result = new ArrayList<>();
+        int count = quantity;
+        while (quantity>0){
+            try(Lot found = sd.lotWithProduct(name,count)){
+                sd.exportProduct(found.getId(),count);
+                result.add(sd.getLot(name,count));
+                quantity = quantity - count;
+                count = quantity;
+            }catch (Exception e){
+                count--;
+            }
         }
+        return result;
     }
 
     private ArrayList<Integer> checkForFreeSpace(Product p,int quantity)throws NotEnoughtSpaceException{
@@ -42,6 +59,7 @@ public class wareHouseController {
         int size = p.getSize() * quantity;
         double weight = p.getWeight() * quantity;
         ArrayList<Integer> lots = new ArrayList<Integer>();
+        //finding available space in slots that have products
         while (quantity >0){
             try(Lot found = sd.findAvailableSpace(size,weight)){
                 sd.importProduct(found.getId(),p.getName(),count);
@@ -58,6 +76,7 @@ public class wareHouseController {
             if(count ==0) break;
         }
         if(quantity !=0) {
+            //if there are any products left put them in a empty slot/s
             while (quantity > 0) {
                 size = p.getSize() * quantity;
                 weight = p.getWeight() * quantity;
--- warehouse/src/interfaces/StockInfo.java
+++ warehouse/src/interfaces/StockInfo.java
@@ -1,6 +1,7 @@
 package interfaces;
 
 import model.Lot;
+import model.Stock;
 
 public interface StockInfo {
     int totalTakenSize();
@@ -8,5 +9,12 @@ public interface StockInfo {
     Lot findAvailableSpace(int size,double weight);
     void importProduct(int lot_id, String product_name, int quantity);
     Lot getFreeLot(int size, double weight);
-    Lot getLotsWithProduct(String name);
+    //query koeto vrashta red ot Stocks s ime i quantity
+    Stock getLot(String name, int quantity);
+    //query vryshta obshto kolko quantity imame ot daden product
+    int productQuantityInStock(String name);
+    //namaliya quantityto na product v lot
+    void exportProduct(int lot_id, int quantity);
+    //namira lot s product s opredeleni broiki
+    Lot lotWithProduct(String name,int quantity);
 }
